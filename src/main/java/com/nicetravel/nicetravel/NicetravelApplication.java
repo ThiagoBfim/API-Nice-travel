af60def2aebecb60ee1cc317e7a363d18070788f
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-02-03 21:59:04 -0200

    Criando o projeto

---
+++ src/main/java/com/nicetravel/nicetravel/NicetravelApplication.java
@@ -0,0 +1,14 @@
+package com.nicetravel.nicetravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicetravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicetravelApplication.class, args);
+	}
+
+}
+
