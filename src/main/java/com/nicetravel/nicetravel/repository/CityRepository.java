f0f33e6c5f98a7bbdfe4f91e83e89649d099bf76
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-02-07 19:41:28 -0200

    [REPOSITORY] - Inclusão do primeiro repository

---
+++ src/main/java/com/nicetravel/nicetravel/repository/CityRepository.java
@@ -0,0 +1,7 @@
+package com.nicetravel.nicetravel.repository;
+
+import com.nicetravel.nicetravel.model.CityEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<CityEntity, Long> {
+}
