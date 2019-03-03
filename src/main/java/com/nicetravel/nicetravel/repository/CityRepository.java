2b32f475bc5e8e3ab4b52f8e0846e55d54e595f9
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-03-03 11:49:38 -0300

    [UPDATE] - Formatando o codigo.

--- src/main/java/com/nicetravel/nicetravel/repository/CityRepository.java
+++ src/main/java/com/nicetravel/nicetravel/repository/CityRepository.java
@@ -4,4 +4,5 @@ import com.nicetravel.nicetravel.model.CityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<CityEntity, Long> {
+
 }
