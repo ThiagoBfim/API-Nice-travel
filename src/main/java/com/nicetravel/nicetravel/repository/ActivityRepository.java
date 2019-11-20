77cbf47d9e4f7b791b1e7407b91459a7acc635d5
Thiago Bomfim <thiagobomfim1995@gmail.com>
2019-11-20 17:52:11 -0300

    [UPDATE] - Correção da logica de buscar as atividades

--- src/main/java/com/nicetravel/nicetravel/repository/ActivityRepository.java
+++ src/main/java/com/nicetravel/nicetravel/repository/ActivityRepository.java
@@ -3,5 +3,9 @@ package com.nicetravel.nicetravel.repository;
 import com.nicetravel.nicetravel.model.ActivityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ActivityRepository extends JpaRepository<ActivityEntity,Long> {
+
+    List<ActivityEntity> findAllByScheduleDayEntityCod(Long scheduleDayId);
 }
